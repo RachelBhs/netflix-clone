37c7d22fae65640c99d453b5502dca805969ebb3
Rachel Bouhalais <rachel.bouhalais@gmail.com>
2022-08-11 16:27:22 +0200

    remove comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,6 @@ module.exports = {
   plugins: [
     require('tailwindcss-textshadow'),
     require('tailwind-scrollbar-hide'),
-    // require('tailwind-scrollbar'),
     require('ps-scrollbar-tailwind'),
   ]
 }
